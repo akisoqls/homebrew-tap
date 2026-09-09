0b3ce61298ff03606510f21f47eaccb0b2ada307
Aki Sakata <akisoqls@gmail.com>
2026-09-09 17:32:30 +0900

    1lm 0.1.2

--- Formula/oneline-memo.rb
+++ Formula/oneline-memo.rb
@@ -2,27 +2,27 @@
 class OnelineMemo < Formula
   desc "CLI for oneline memo (1lm)"
   homepage "https://oneline-memo.akisoqls.deno.net"
-  version "0.1.1"
+  version "0.1.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.1/1lm-0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "c88d4e72bc6e45a47c4a10e0295af59c5d283cdc825cf6f0951b7310da2cbbb7"
+      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.2/1lm-0.1.2-aarch64-apple-darwin.tar.gz"
+      sha256 "a1c6cba59c41368d31f2b0ae60a88435a4db97a522341c7610cb2e2f9d3217e5"
     end
     on_intel do
-      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.1/1lm-0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "b542e064a055cf0612be471c6eae986193f4223de46805507e60e4ecdc7777b7"
+      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.2/1lm-0.1.2-x86_64-apple-darwin.tar.gz"
+      sha256 "179e84fe4ce119d9c5d3ebf5942c85799e174c134526a109a542a8a0bdce3dd4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.1/1lm-0.1.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "40c335315f2e92059c04a94d78293860576fd2f1ea6b83f8b3ada8a4dc2e1734"
+      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.2/1lm-0.1.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "86bf3dca80bf931eabec9936b544b0c665611141959b40802995c1f05e795023"
     end
     on_intel do
-      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.1/1lm-0.1.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "308f73e5ab68d37d370ad9f77c5da271ace16182b2642d38ef6b4302ed2b888c"
+      url "https://github.com/akisoqls/homebrew-tap/releases/download/v0.1.2/1lm-0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4f5e8d4d6dd57784ea064ce4c9b663ddcd8af760c08a19387e955c9e89fd74a9"
     end
   end
 
